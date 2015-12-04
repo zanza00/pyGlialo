ebb9c5d63ab7b8b1dfaf3052fc7696a92e99dfc9
Zanza <zanza00@gmail.com>
2015-12-04 23:34:18 +0000

    versione che funziona su cloud9.io

--- flask_pyGlialo.py
+++ flask_pyGlialo.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 from pyGlialo import *
+import os
 
 app = Flask(__name__)
 
@@ -67,4 +68,8 @@ def reset_app():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        debug = True,
+        host = os.getenv('IP', '0.0.0.0'),
+        port = int(os.getenv('PORT', '8080'))
+        )
